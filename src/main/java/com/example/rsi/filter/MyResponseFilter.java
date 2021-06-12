5e343fbcae96c93dabfb2343ea4f27fc281f97f8
Mariusz Grynczel <mariusz.grynczel@gmail.com>
2021-06-12 22:17:29 +0200

    poprawki filtru

--- src/main/java/com/example/rsi/filter/MyResponseFilter.java
+++ src/main/java/com/example/rsi/filter/MyResponseFilter.java
@@ -5,8 +5,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.Date;
 
 @WebFilter("/*")
 public class MyResponseFilter implements Filter {
@@ -14,9 +18,14 @@ public class MyResponseFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.setHeader("User", authentication.getName());
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        Writer fileWriter = new FileWriter("logs.txt", true);
+        fileWriter.write(authentication.getName() + ", " + authentication.getAuthorities().toString() + ", " + new Date() + ", url: " + httpServletRequest.getRequestURI() + ", method: " + httpServletRequest.getMethod() + ", status: " + httpServletResponse.getStatus() + "\n");
+        fileWriter.close();
+
+        httpServletResponse.setHeader("Role", authentication.getName());
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
